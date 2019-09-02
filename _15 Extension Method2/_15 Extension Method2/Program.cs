106da9e99134d9c410b2548a1a1943e7fd9e5dcc
progquartz <progquartz@gmail.com>
2019-09-03 03:33:59 +0900

    _15 extension Method 2

---
+++ _15 Extension Method2/_15 Extension Method2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Extension_Method2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            System.Linq.Enumerable 클래스는 LINQ 쿼리에서 사용되는 많은 확장 메서드들을 포함하는 클래스이다. 
+            한 예를 들어, Enumerable 클래스는 다음과 같은 Where() 확장메서드를 포함하고 있다. 
+            첫번째 파라미터는 이 메서드가 IEnumerable<T> 인터페이스를 지원하는 모든 Type에 사용된다는 것을 의미한다.
+            두번째 파라미터는 Func 라는 Delegate를 받아들인다는 것을 의미하는데, 보통 여기에는 LINQ 쿼리를 람다식 (Lambda Expression)으로 표현하여 넣게 된다. 
+             */
+        }
+
+    }
+}
---
+++ _15 Extension Method2/_15 Extension Method2/_15 plus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Extension_Method2
+{
+    public static class _15_plus
+    {
+        public static IEnumerable<TSource> Where<TSource>(
+     this IEnumerable<TSource> source)
+    }
+}
